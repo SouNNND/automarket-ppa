0980ae545f8f487922d9e9ce1ee05a3a6aac65dd
Alex Raducu <alexsnd33@gmail.com>
2021-04-17 17:03:43 +0300

    Design MainActivity and LoginActivity with intent

--- app/src/main/java/com/bar/automarket/MainActivity.java
+++ app/src/main/java/com/bar/automarket/MainActivity.java
@@ -2,13 +2,26 @@ package com.bar.automarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).hide(); // hide the title bar
+    }
+
+    public void launchLoginActivity(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }

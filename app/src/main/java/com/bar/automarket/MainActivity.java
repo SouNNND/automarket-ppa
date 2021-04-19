68d9320e1a50bb0b100e9ed81009bb39c8adbccf
Alex Raducu <alexsnd33@gmail.com>
2021-04-19 18:13:19 +0300

    Made register and login work

--- app/src/main/java/com/bar/automarket/MainActivity.java
+++ app/src/main/java/com/bar/automarket/MainActivity.java
@@ -8,20 +8,33 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Objects.requireNonNull(getSupportActionBar()).hide(); // hide the title bar
+
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
     public void launchLoginActivity(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if(user != null) {
+            //user is signed in
+            startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+        } else {
+            //user is not signed in
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
     }
+
 }
